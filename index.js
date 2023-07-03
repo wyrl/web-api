0c84a8617a2672e9eda97c3ca3882a7e5ca1f246
wyrlvillazorda <wyrlvillazorda@gmail.com>
2023-07-03 10:18:45 +0800

    add authorization

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const data = require('./data.json');
-const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const port = 8080;
+const data = require("./data.json");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const fakeAuthorization = "dGVzdDpzZWNyZXQ=";
 
-app.get('/list', (req, res) => {
+const CODE_UNAUTHORIZED = 401;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/list", (req, res) => {
   res.json(data);
-})
+});
 
-app.post('/add', (req, res) => {
+app.post("/add", (req, res) => {
+  const request_header = req.headers["authorization"];
 
   const body = req.body;
-  data.push(body);
 
-  res.send("Added Successfully.");
+  if (request_header == fakeAuthorization) {
+    data.push(body);
+    res.send("Added Successfully.");
+  } else {
+    res.status(CODE_UNAUTHORIZED);
+    res.send("Unauthorized");
+  }
 });
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
