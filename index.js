f1195919f9b33912da7dc7932d0b21350067b910
Wyr' Villazorda <wyrlvillazorda@gmail.com>
2023-07-03 09:38:20 +0800

    update for post request

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8080
 const data = require('./data.json');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send(data)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/list', (req, res) => {
+  res.json(data);
 })
+
+app.post('/add', (req, res) => {
+
+  const body = req.body;
+  data.push(body);
+
+  res.send("Added Successfully.");
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
